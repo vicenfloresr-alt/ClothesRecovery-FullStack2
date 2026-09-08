a9c1fe58c99391ee3986d0e848386f1c2bc13923
vicenfloresr-alt <vicen.floresr@duocuc.cl>
2026-09-08 16:59:09 -0300

    Delete resenas directory

--- resenas/src/main/java/com/clothesrecovery/resenas/service/ResenaDuplicadaException.java
+++
@@ -1,9 +0,0 @@
-package com.clothesrecovery.resenas.service;
-
-public class ResenaDuplicadaException extends RuntimeException {
-
-    public ResenaDuplicadaException(String mensaje) {
-        super(mensaje);
-    }
-
-}
