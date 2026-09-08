6c489677f82d08be65a3eb1084798fb6e50e7466
vicenfloresr-alt <vicen.floresr@duocuc.cl>
2026-09-08 16:59:44 -0300

    Delete inventario directory

--- inventario/src/main/java/com/clothesrecovery/inventario/service/StockInsuficienteException.java
+++
@@ -1,9 +0,0 @@
-package com.clothesrecovery.inventario.service;
-
-public class StockInsuficienteException extends RuntimeException {
-
-    public StockInsuficienteException(String mensaje) {
-        super(mensaje);
-    }
-
-}
