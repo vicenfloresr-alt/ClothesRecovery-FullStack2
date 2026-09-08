2ded8cf4a6c2dcf30f351984801be075461ace47
vicenfloresr-alt <vicen.floresr@duocuc.cl>
2026-09-08 17:00:01 -0300

    Delete devoluciones directory

--- devoluciones/src/main/java/com/clothesrecovery/devoluciones/service/EstadoInvalidoException.java
+++
@@ -1,9 +0,0 @@
-package com.clothesrecovery.devoluciones.service;
-
-public class EstadoInvalidoException extends RuntimeException {
-
-    public EstadoInvalidoException(String mensaje) {
-        super(mensaje);
-    }
-
-}
